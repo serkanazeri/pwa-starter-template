fd1f934aa519770bf598262acbeb003d859796f6
Serkan Azeri <serkanazeri@gmail.com>
2023-03-14 01:15:20 +0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+module.exports = nextConfig;
